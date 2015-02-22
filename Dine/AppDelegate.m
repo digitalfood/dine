4edfe63037cde82395ff6c2e9b619da036250ff4
Matt Ho <mattho@yahoo-inc.com>
2015-02-22 15:59:09 -0800

    Add settings view

--- Dine/AppDelegate.m
+++ Dine/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <FacebookSDK/FacebookSDK.h>
 #import "Parse/Parse.h"
 #import <ParseUI/ParseUI.h>
 //#import <ParseCrashReporting/ParseCrashReporting.h>
@@ -73,7 +74,7 @@ NSString *kClientKey = @"iJbIfAr9JJ8Td5JOkZw8zTTlh9UlF7cwHdxt0x5g";
     PFLogInViewController *logInController = [[PFLogInViewController alloc] init];
     logInController.delegate = self;
     logInController.fields = (PFLogInFieldsFacebook);
-    
+    [logInController setFacebookPermissions:[NSArray arrayWithObjects:@"public_profile", @"email", @"user_friends", nil]];
     CGFloat width = [[UIScreen mainScreen] bounds].size.width;
     CGFloat height = [[UIScreen mainScreen] bounds].size.height;
     
@@ -117,6 +118,18 @@ NSString *kClientKey = @"iJbIfAr9JJ8Td5JOkZw8zTTlh9UlF7cwHdxt0x5g";
 }
 
 - (void)signedIn {
+    FBRequest *request = [FBRequest requestForMe];
+    
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            NSDictionary *userData = (NSDictionary *)result;
+            PFUser *user = [PFUser currentUser];
+            user[@"fullname"] = userData[@"name"];
+            user[@"profileImageUrl"] = [NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", userData[@"id"]];
+        }
+    }];
+    
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     MainViewController *vc = [[MainViewController alloc] init];
     self.window.rootViewController = vc;
