ab2fe3f5c420b7b72562dd0149b1da23eeefae22
Fabian Uribe <uribe@yahoo-inc.com>
2015-02-22 18:17:53 -0800

    Add request cash method

--- Dine/checkSplitViewController.m
+++ Dine/checkSplitViewController.m
@@ -54,8 +54,23 @@
 }
 
 - (IBAction)onRequestBtn:(id)sender {
-
+    
+    NSString *recipientEmail = @"fabian.uribe@gmail.com";
+    NSString *serviceEmail = @"request@square.com";
+    
+    
+    NSString *recipients = [NSString stringWithFormat:@"mailto:%@?cc=%@&subject=$%.02f",recipientEmail, serviceEmail, self.splitAmount];
+    
+    NSString *body = [NSString stringWithFormat:@"&body=Please send $%0.2f to cover your share of the total bill ($%0.2f) from %@", self.splitAmount, self.amount, self.restaurant.name];
+    
+    NSString *email = [NSString stringWithFormat:@"%@%@", recipients, body];
+    
+    email = [email stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:email]];
 }
+
+
 
 
 @end
